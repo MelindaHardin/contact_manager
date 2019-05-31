e8120c6c2c883a3eb5b2775b1bb12dab62b9aa68
Melinda H <melinda.yw.hardin@gmail.com>
2019-05-30 21:17:16 -0500

    cards

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Contact from "./components/Contact";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header branding="Contact Manager" />
+        <div class="container">
+          <Contact
+            name="John Doe"
+            email="Jman@email.com"
+            phone="222-222-2222"
+          />
+          <Contact
+            name="Jane Doe"
+            email="Jlady@email.com"
+            phone="333-333-3333"
+          />
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
